1a0612fd7a041bd4c5e4e27076c872a23f47cf3c
ghousdar <hanifghous@gmail.com>
2023-06-22 12:51:31 +0500

    fix bug

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import View1 from "./views/view1/View1";
 
 const routes = [
     {
-        path:"view1", name:"View One",element: View1
+        path:"", name:"View One",element: View1
     },
 ];
 
