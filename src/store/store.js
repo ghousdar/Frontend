ea720b77dc8423a3ff0ce50be2ff4ab6a79762a0
ghousdar <hanifghous@gmail.com>
2023-06-22 12:50:07 +0500

    fixed

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,14 @@
 import { configureStore} from "@reduxjs/toolkit"
-
+import authReducer from "./AuthenticationSlice"
 
 
 
 const store = configureStore ({
 
     reducer: {
+
+        auth: authReducer
+    
           // Configure your stor here
 
     }
